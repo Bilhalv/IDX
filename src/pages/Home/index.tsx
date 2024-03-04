53c2cfaf2c73d4373030db9fe0580eca8b13dd1d
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-03-04 18:10:34 +0000

    style: better header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,9 +8,10 @@ const Home = () => {
     <>
       <body className="bg-purple-1 min-h-screen text-white">
         <header className="bg-purple-3 p-4">
-          <h1 className="text-4xl font-bold mb-4">Welcome to my portfolio</h1>
-          <p className="text-xl">Here are some of the technologies I have been working with:</p>
+          <h1 className="text-4xl font-bold mb-4 text-center">Explore My Portfolio</h1>
+          <p className="text-xl text-center">Here are some projects I've been developing:</p>
         </header>
+
         <div className="flex justify-center items-center h-1/2 flex-col gap-4 mt-4">
           {projects.map((project: Project, index: number) => (
             <Card key={index} {...project} />
