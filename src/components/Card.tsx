d2896d252ee1f9ac2b121daa4194649823ab3f6b
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-03-05 09:31:27 -0300

    fix img broken

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ export function Card(props: Project) {
         <div className="bg-purple-2 rounded-lg shadow-lg p-4 w-1/2">
             <h2 className="text-xl font-semibold text-center">{props.title}</h2>
             <a href={props.link} target="_blank" rel="noreferrer">
-                <img src={props.img} alt={props.title} className="w-full h-48 object-fit rounded-lg my-2" />
+                <img src={props.img} alt={props.title} className="w-full h-48 object-cover rounded-lg my-2" />
             </a>
             <p className="italic">&nbsp;&nbsp;&nbsp;&nbsp;{props.desc}</p>
         </div>
